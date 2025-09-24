6d52005fa117cd0a9f01ac22af7dd7f7e80d8142
Camily <tctorres@espol.edu.ec>
2025-09-23 23:59:00 -0500

    Clase base para tests con anotaciones para configurar y abrir el driver, además de cerrar el driver al finalizar

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,4 +1,24 @@
 package tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import pages.PianoPage;
+
 public class BaseTest {
+    protected WebDriver driver;
+    protected PianoPage pianoPage;
+    @BeforeMethod
+    public void beforeMethod(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        pianoPage = new PianoPage(driver, "https://www.musicca.com/es/piano");
+    }
+    @AfterMethod
+    public void afterMethod(){
+        driver.close();
+    }
 }
